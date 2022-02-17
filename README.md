8c183efdfebfe72cf93f50186bdb1f5f808b2520
Nicholas Pfister <npfister13@hotmail.com>
2022-02-17 14:57:48 -0500

    added functioning contact form

--- README.md
+++ README.md
@@ -85,3 +85,5 @@ npm i --save @fortawesome/fontawesome-svg-core
 npm install --save @fortawesome/free-solid-svg-icons
 
 npm install --save @fortawesome/react-fontawesome
+
+npm i emailjs-com
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import NavTab from "./NavTab";
+import { init } from '@emailjs/browser'
+import { send } from 'emailjs-com'
+init("user_fWsJfb5HpUJl4vKgJHZi3");
 
+
+
+// service_4av0u3s
 export default function Contact() {
+
+    const [contactInfo, setContactInfo] = useState({
+        from_name: "",
+        to_name: 'Nicholas Pfister',
+        message: "",
+        reply_to: "",
+    });
+
+    const handleChange = (event) => {
+        setContactInfo({ ...contactInfo, [event.target.name]: event.target.value });
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(contactInfo);
+        send(
+            'service_4av0u3s',
+            'template_fyb87tb',
+            contactInfo,
+            'user_fWsJfb5HpUJl4vKgJHZi3'
+        )
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text)
+            })
+            .catch((err) => {
+                console.log('FAILED...', err);
+            });
+        setContactInfo({ from_name: "", reply_to: "", message: "" })
+    }
 
     return(
         <React.Fragment>
             <NavTab/>
+            <div className="contact">
+                <div className="container">
+                    <h1>Get in contact</h1>
+                    <div className="row row-content">
+                        <div className="col-6 m-auto">
+                            <form id="contact-form" onSubmit={handleSubmit} method="POST">
+                                <div className="form-group">
+                                    <label htmlFor="name">Name</label>
+                                    <input type="text" name="from_name" className="form-control" placeholder="Your Name" value={contactInfo.from_name} onChange={handleChange} />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputEmail1">Email address</label>
+                                    <input type="email" name="reply_to" className="form-control" placeholder="Your Email" aria-describedby="emailHelp" value={contactInfo.reply_to} onChange={handleChange} />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="message">Message</label>
+                                    <textarea className="form-control" name="message" rows="5" placeholder="Message" value={contactInfo.message} onChange={handleChange}></textarea>
+                                </div>
+                                <div className="subBtn col-span-2 align-self-end">
+                                    <button type="submit" className="btn btn-primary btn-lg">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     )
 
