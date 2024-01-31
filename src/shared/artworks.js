1d2814b3ec105226220d8ed070082bd6c40a77bb
Nicholas Pfister <herpaderpsherbert@hotmail.com>
2024-01-31 17:54:59 -0500

    Updated Artworks section

--- src/shared/artworks.js
+++ src/shared/artworks.js
@@ -1,80 +1,103 @@
 export const ARTWORKS = [
-    
     {
-        name: 'Vaporheroes Sprites',
-        image: './assets/images/vaporheroes-new-sprites.png',
-        alt: "Final Fantasy 1-3 inspired sprites"
+        name: "Vaporheroes Sprites",
+        image: "./assets/images/vaporheroes-new-sprites.png",
+        alt: "Final Fantasy 1-3 inspired sprites",
     },
     {
-        name: 'Placeholder 8',
-        image: './assets/images/touhou-square.gif',
-        alt: 'Touhou animation with Reimu Hakurei chasing Sakuya Izayoi'
+        name: "Touhou Fanart",
+        image: "./assets/images/touhou-square.gif",
+        alt: "Touhou animation with Reimu Hakurei chasing Sakuya Izayoi",
     },
     {
-        name: 'Placeholder 3',
-        image: './assets/images/new pokemon old sprites square.png',
-        alt: "New Pokemon Sprigatito, Quaxly, and Fuecoco in an old Pokemon sprite style"
+        name: "Pokemon Fanart",
+        image: "./assets/images/new pokemon old sprites square.png",
+        alt: "New Pokemon Sprigatito, Quaxly, and Fuecoco in an old Pokemon sprite style",
     },
-    
+
     {
-        name: 'Placeholder 6',
-        image: './assets/images/when-creating-a-meme-goes-right.gif',
-        alt: "Nick at desk animation"
-    },
-    {
-        name: 'Placeholder 5',
-        image: './assets/images/alpha-brb-stream-animation.gif',
-        alt: "Alpha at desk animation"
+        name: "Nick at his desk",
+        image: "./assets/images/when-creating-a-meme-goes-right.gif",
+        alt: "Nick at desk animation",
     },
     {
-        name: 'Placeholder 4',
-        image: './assets/images/desk-brb-animation.gif',
-        alt: "Grace at desk animation"
+        name: "Alpha BRB Stream Screen",
+        image: "./assets/images/alpha-brb-stream-animation.gif",
+        alt: "Alpha at desk animation",
     },
     {
-        name: 'Placeholder 2',
-        image: './assets/images/darin at desk final.gif',
-        alt: "Darin at desk animation"
+        name: "turiansgonewild BRB Stream Screen",
+        image: "./assets/images/desk-brb-animation.gif",
+        alt: "Grace at desk animation",
     },
     {
-        name: 'Placeholder 7',
-        image: './assets/images/ram-skull.png',
-        alt: "1-bit pixel art of a ram skull"
+        name: "Darin at his desk",
+        image: "./assets/images/darin at desk final.gif",
+        alt: "Darin at desk animation",
     },
     {
-        name: 'Placeholder 1',
-        image: './assets/images/majora mask arid palette instagram.jpeg',
-        alt: "Majora's Mask with a limited palette"
+        name: "1-bit Ram Skull",
+        image: "./assets/images/ram-skull.png",
+        alt: "1-bit pixel art of a ram skull",
     },
     {
-        name: 'Placeholder 9',
-        image: './assets/images/hyper light breaker.gif',
-        alt: 'Hyper Light Breaker Fanart Animation'
-    },
-    
-    {
-        name: 'Money Bag',
-        image: './assets/images/money bag plop.gif',
-        alt: "Money Bag Dropping Commission"
+        name: "Majora Mask Limited Palette",
+        image: "./assets/images/majora mask arid palette instagram.jpeg",
+        alt: "Majora's Mask with a limited palette",
     },
     {
-        name: 'turiansgonewild Airplane Screen',
-        image: './assets/images/Airplane Screen.gif',
-        alt: '@turiansgonewild Airplane Screen Commission'
+        name: "Hyper Light Breaker Fanart in Hyper Light Drifter Style",
+        image: "./assets/images/hyper light breaker.gif",
+        alt: "Hyper Light Breaker Fanart Animation",
     },
     {
-        name: 'Mob Psycho 100',
-        image: './assets/images/mob 100 final.gif',
-        alt: 'Mob Psycho 100 Fanart Animation'
+        name: "Money Bag",
+        image: "./assets/images/money bag plop.gif",
+        alt: "Money Bag Dropping Commission",
     },
     {
-        name: 'Chainsaw Man',
-        image: './assets/images/poses2.png',
-        alt: 'Chainsaw Man Fanart'
+        name: "turiansgonewild Airplane Screen",
+        image: "./assets/images/Airplane Screen.gif",
+        alt: "@turiansgonewild Airplane Screen Commission",
     },
     {
-        name: 'Taco Galaxy',
-        image: './assets/images/canvas ver 3 w bg.png',
-        alt: 'Taco Galaxy'
+        name: "Mob Psycho 100",
+        image: "./assets/images/mob 100 final.gif",
+        alt: "Mob Psycho 100 Fanart Animation",
     },
-]
+    {
+        name: "Chainsaw Man",
+        image: "./assets/images/poses2.png",
+        alt: "Chainsaw Man Fanart",
+    },
+    {
+        name: "Taco Galaxy",
+        image: "./assets/images/canvas ver 3 w bg.png",
+        alt: "Taco Galaxy",
+    },
+    {
+        name: "Frisk Knife Attack Animation",
+        image: "./assets/images/swings faster.gif",
+        alt: "Black and white animation of a character swinging a knife",
+    },
+    {
+        name: "Komi Can't Communicate Fanart",
+        image: "./assets/images/komi.png",
+        alt: "Komi Can't Communicate Fanart",
+    },
+    {
+        name: "Tekken 8 Reina Fanart",
+        image: "./assets/images/reina doodles 1.1.png",
+        alt: "Tekken 8 Reina",
+    },
+    {
+        name: "Lethal Company Fanart Animation",
+        image: "./assets/images/Lethal Company.gif",
+        alt: "Lethal Company Fanart Animation",
+    },
+    {
+        name: "Lethal Company Ship Fanart Animation",
+        image: "./assets/images/lethal company ship.gif",
+        alt: "Lethal Company Ship Fanart Animation",
+    },
+];
